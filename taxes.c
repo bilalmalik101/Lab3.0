a9e26cfc6fe88fc34e9ee8a77547626029f11127
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-21 18:17:44 +0500

    using if else statement to find taxes between 19901 to 81050

--- taxes.c
+++ taxes.c
@@ -33,7 +33,11 @@ int main(int argc, char **argv) {
     tax=0+0.1*(agi-0);
     childCredit=numChildren*2000;
     totalTax=tax-childCredit;
-   }
+   } else if(agi>=19901 && agi<=81050){
+    tax=1990+0.12*(agi-19900);
+    childCredit=numChildren*2000;
+    totalTax=tax-childCredit;
+  } 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
   printf("Child Credit: $%10.2f\n", childCredit);
